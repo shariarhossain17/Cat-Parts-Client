5120f759f9189e202d08ac819267fb4e89057931
shariarhossain23e <shariarhossain23@gamil.comm>
2022-05-22 01:39:09 +0600

    secure firebase key

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Home from './Pages/Home/Home';
+import Login from './Pages/Login/Login';
 
 function App() {
 
@@ -9,6 +10,7 @@ function App() {
       
       <Routes>
         <Route path='/'element={<Home/>}></Route>
+        <Route path='/login'element={<Login></Login>}></Route>
       </Routes>
     </div>
   );
