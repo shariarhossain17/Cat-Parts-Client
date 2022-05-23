8aa5c0236f20ca2d42b2912d1cf2aa76daa3978c
shariarhossain23e <shariarhossain23@gamil.comm>
2022-05-23 13:39:54 +0600

    review post card

--- src/Pages/Dashboard/AddReview.js
+++ src/Pages/Dashboard/AddReview.js
@@ -1,12 +1,53 @@
-import React from 'react';
-import PageTitle from '../Shared/PageTitle';
+import React, { useState } from "react";
+import { useAuthState } from "react-firebase-hooks/auth";
+import ReactStars from "react-rating-stars-component";
+import auth from "../../firebase.init";
+import PageTitle from "../Shared/PageTitle";
 
 const AddReview = () => {
-    return (
-        <div>
-            <PageTitle title={"add-review"}></PageTitle>
+  const [star, setStar] = useState("");
+  const [comment,setComment] = useState("")
+  const [user] = useAuthState(auth)
+  const ratingChanged = (newRating) => {
+    setStar(newRating);
+  };
+  const handleReview = (event) =>{
+      event.preventDefault()
+      const review = {
+        comment:comment,
+        star:star,
+        userName:user?.displayName,
+        email:user?.email
+      }
+      console.log(review);
+  }
+  return (
+    <div>
+      <PageTitle title={"add-review"}></PageTitle>
+
+      <div class="card max-w-sm mx-auto mt-6 bg-base-100 shadow-xl">
+        <div class="card-body">
+          <h2 class="card-title mb-2">Add Review</h2>
+          <textarea
+            onChange={(e)=> setComment(e.target.value)}
+            class="textarea textarea-bordered"
+            placeholder="Comment"
+          ></textarea>
+          <ReactStars
+            count={5}
+            onChange={ratingChanged}
+            size={40}
+            isHalf={true}
+            emptyIcon={<i className="far fa-star"></i>}
+            halfIcon={<i className="fa fa-star-half-alt"></i>}
+            fullIcon={<i className="fa fa-star"></i>}
+            activeColor="#faca51"
+          />
         </div>
-    );
+        <button onClick={handleReview} className="bg-[#f57224] text-white p-4">Review</button>
+      </div>
+    </div>
+  );
 };
 
 export default AddReview;
