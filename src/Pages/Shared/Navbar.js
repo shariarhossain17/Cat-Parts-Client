58fe5734ebc4d44ddc3963e9cca34280b8c94be8
shariarhossain23e <shariarhossain23@gamil.comm>
2022-05-25 17:39:57 +0600

    navbar style

--- src/Pages/Shared/Navbar.js
+++ src/Pages/Shared/Navbar.js
@@ -1,8 +1,10 @@
 import { signOut } from "firebase/auth";
 import React from "react";
 import { useAuthState } from "react-firebase-hooks/auth";
-import { NavLink, useLocation } from "react-router-dom";
+import { useLocation } from "react-router-dom";
+import logo3 from "../../Assets/logo3.jpeg";
 import auth from "../../firebase.init";
+import CustomLink from "./CustomLink";
 
 const Navbar = () => {
   const [user] = useAuthState(auth);
@@ -13,29 +15,33 @@ const Navbar = () => {
 
   const menuItem = (
     <>
-      <li>
-        <NavLink to="/">Home</NavLink>
-      </li>
+      <a>
+        <CustomLink to="/">Home</CustomLink>
+      </a>
       {user && (
-        <li>
-          <NavLink to="/dashboard">Dashboard</NavLink>
-        </li>
+        <a>
+          <CustomLink to="/dashboard">Dashboard</CustomLink>
+        </a>
       )}
+        <a className="bg-[#ff4400] px-4  py-2 text-white rounded-full font-[500]">
+        <p>{user?.displayName}</p>
+      </a>
       {user ? (
         <button onClick={logOut} className="btn btn-ghost">
           logout
         </button>
       ) : (
-        <li>
-          <NavLink to="/login">Login</NavLink>
-        </li>
+        <a>
+          <CustomLink to="/login">Login</CustomLink>
+        </a>
       )}
+     
     </>
   );
   return (
     <div className="lg:px-12 mt-16 ">
       <div class="navbar fixed top-0 z-50 left-0  bg-gray-50">
-        <div class="navbar-start">
+        <div class="navbar-start w-full">
           <div class="dropdown">
             <label tabindex="0" class="bg-[#ff4400] lg:hidden">
               <svg
@@ -55,18 +61,21 @@ const Navbar = () => {
             </label>
             <ul
               tabindex="0"
-              class="menu menu-compact flex items-center dropdown-content mt-5 p-2 shadow bg-gray-50 w-[97vw] text-black"
+              class="menu menu-compact flex items-center dropdown-content mt-5 p-2 shadow bg-gray-50 w-[97vw] text-black gap-3"
             >
               {menuItem}
             </ul>
           </div>
-          <a class="btn btn-ghost normal-case text-xl text-black">daisyUI</a>
+          <div className="flex items-center justify-start">
+            <img src={logo3} width="100px" alt="" />
+           <h2 className="lg:text-4xl font-bold uppercase">Car Parts <span className="text-[#ff4400]">Shop</span></h2>
+          </div>
         </div>
         {pathname.includes("dashboard") && (
           <label for="my-drawer-2" class=" lg:hidden">
             <svg
               xmlns="http://www.w3.org/2000/svg"
-              class="h-6 w-6 text-white ml-auto"
+              class="h-6 w-6 text-black ml-auto"
               fill="none"
               viewBox="0 0 24 24"
               stroke="currentColor"
@@ -80,8 +89,8 @@ const Navbar = () => {
             </svg>
           </label>
         )}
-        <div class="navbar-end hidden text-black lg:flex">
-          <ul class="menu menu-horizontal p-0">{menuItem}</ul>
+        <div class="navbar-end hidden text-gray-800 font-[500] lg:flex">
+          <ul class="menu menu-horizontal p-0 items-center gap-6">{menuItem}</ul>
         </div>
       </div>
     </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,35 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
-  theme: {
-    extend: {},
+  daisyui: {
+    themes: [
+      {
+        light: {
+          primary: "#ff4400",
+          secondary: "#FFC000",
+          accent: "#FFEDD5",
+          neutral: "#F3F4F6",
+          "base-100": "#FFFFFF",
+          info: "#98A8DD",
+          success: "#1BBB70",
+          warning: "#DF7E07",
+          error: "#FA5C5C",
+        },
+      },
+      {
+        dark: {
+          primary: "#D9F99D",
+          secondary: "#FDE68A",
+          accent: "#4B5563",
+          neutral: "#F3F4F6",
+          "base-200": "#1F2937",
+          info: "#98A8DD",
+          success: "#1BBB70",
+          warning: "#F59E0B",
+          error: "#FB7185",
+        },
+      },
+      
+    ],
   },
   plugins: [require("daisyui")],
 }
